0e45ff6f1315a6aecad3539dc2f047a633edcd79
Luca <98969530+LucaCavioni@users.noreply.github.com>
2022-07-06 09:22:55 +0200

    noteboock gia completametne runnati + get_data() ha path come param

--- src/data/carica_dati.py
+++ src/data/carica_dati.py
@@ -3,9 +3,9 @@ import sys, os
 sys.path.append('src')
 from feature.pulizia_dati import pulisci_df, preprocessing
 
-def get_data():    
+def get_data(path:str='dati\WA_Fn-UseC_-Telco-Customer-Churn.csv'):    
     # carico i dati dal csv contenuto nella rep locale
-    df = pd.read_csv('dati\WA_Fn-UseC_-Telco-Customer-Churn.csv')
+    df = pd.read_csv(path)
     # df = pulisci_df(df)
     df = preprocessing(df)
     return df
